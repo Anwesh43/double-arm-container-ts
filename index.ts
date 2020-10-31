2f24f45ff4f0e63a413a653497f61c9c18b664b5
Anwesh43 <anweshthecool0@gmail.com>
2020-10-31 18:04:07 +0530

    adding DACNode class

--- index.ts
+++ index.ts
@@ -159,3 +159,45 @@ class Animator {
         }
     }
 }
+
+class DACNode {
+
+    prev : DACNode 
+    next : DACNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new DACNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawDACNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : DACNode {
+        var curr : DACNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
