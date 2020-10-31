bedcf44e1a81ce5e7f9a1e0f55fe87fd3be5ab39
Anwesh43 <anweshthecool0@gmail.com>
2020-10-31 18:14:44 +0530

    adding renderer in stage

--- index.ts
+++ index.ts
@@ -87,22 +87,27 @@ class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D 
+    renderer : Renderer 
 
     initCanvas() {
         this.canvas.width = w 
         this.canvas.height = h 
         this.context = this.canvas.getContext('2d')
         document.body.appendChild(this.canvas)
+        this.renderer = new Renderer()
     }
 
     render() {
         this.context.fillStyle = backColor 
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
