9f347fa56bfef623b95e48490edbeb1668ec38d9
Anwesh43 <anweshthecool0@gmail.com>
2020-10-31 18:48:19 +0530

    adding fixes

--- index.ts
+++ index.ts
@@ -33,6 +33,9 @@ class ScaleUtil {
 class DrawingUtil {
 
     static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        if (Math.abs(x1 - x2) <= 0.1 && Math.abs(y1 - y2) <= 0.1) {
+            return
+        }
         context.beginPath()
         context.moveTo(x1, y1)
         context.lineTo(x2, y2)
