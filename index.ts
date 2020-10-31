2980dd274d7efd5c527b887d95c7c647342843c7
Anwesh43 <anweshthecool0@gmail.com>
2020-10-31 16:52:33 +0530

    adding constatns

---
+++ index.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 7
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 4.9 
+const armWidthFactor : number = 15.2 
+const delay : number = 20
+const backColor : string = "#BDBDBD"
+const colors : Array<string> = [
+    "#F44336",
+    "#4CAF50",
+    "#3F51B5",
+    "#009688",
+    "#FF9800"
+] 
